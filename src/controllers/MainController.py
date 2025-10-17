b621bc7511fa757b48c8b39e0f0c0b9249ed162d
ariqhikari <ahidayatbia@gmail.com>
2025-10-17 17:06:55 +0700

    feat: /predict-image

--- src/controllers/MainController.py
+++ src/controllers/MainController.py
@@ -1,18 +1,23 @@
 
 from flask import Blueprint
-from flask import request,g
+from flask import request, g, jsonify
 from src.services.CleanDataServices import CleanDataService
 from src.services.CreateScreenshoot import ScreenshotDataService
 from src.services.PredictDataServices import PredictDataService
 from src.services.HITLServices import HITLService
 import src.utils.getResponse as Response  
 import os
+import re
+from groq import Groq
 
 MainApp = Blueprint('MainApp', __name__,)
 cleanDataService =  CleanDataService()
 screenshotService = ScreenshotDataService()
 predictDataService = PredictDataService()
 hitlService = HITLService()
+
+client = Groq(api_key="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")
+
 @MainApp.route('/test', methods=['POST'])
 def test():
     return Response.success([],"Test post endpoint is working")
@@ -103,3 +108,48 @@ def seed_dataset():
     if(result['status'] == 'failed'):
         return Response.error(result['data'],result['code'])
     return Response.success(result['data'],"success create seed dataset")
+
+@MainApp.route('/predict-image', methods=['POST'])
+def predict_image():
+    print("INI MASUK SINI!!!!!!!!!!!!!")
+    try:
+        data = request.json
+        image_url = data.get('image_url')
+
+        print("Ini IMAGE URL nya: ", image_url)
+        
+        global client
+        completion = client.chat.completions.create(
+            model="llama-3.2-90b-vision-preview",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Is this image related to porn or adult content? If it is porn or adult content, answer with 'porn' only; if it is not porn, answer with 'np'"
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": image_url
+                            }
+                        }
+                    ]
+                }
+            ],
+            temperature=0.1,
+            max_tokens=3,
+            top_p=1,
+            stream=False,
+            stop=None,
+        )
+
+        # Mengubah teks menjadi lowercase
+        text = completion.choices[0].message.content.lower()
+        # Menghapus tanda baca menggunakan regex
+        text = re.sub(r'[^\w\s]', '', text)
+        return jsonify({"hasil": text})
+    except Exception as e:
+        print("INI ERROR : ", e)
+        return jsonify({"error": str(e)}), 500
